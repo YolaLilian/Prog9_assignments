a0030274c3ffc6bdbc639c64ebf70dfcb7d08578
Lilian Kempees <45563232+YolaLilian@users.noreply.github.com>
2021-03-31 11:21:14 +0200

    Did a lot (almost done!)

--- pwa/js/app.js
+++ pwa/js/app.js
@@ -1,18 +1,63 @@
 
+// Imports
+import localForage from "localforage";
+import 'regenerator-runtime/runtime';
+
 // Variables
+const status = document.querySelector(".status");
 const ul = document.getElementById('games');
+const ul2 = document.getElementById('tags');
 const jsonButton = document.getElementById('json-button');
 const jsonButton2 = document.getElementById('json-button_2');
 
 // Event Listeners
 jsonButton.addEventListener('click', fetchJSON);
-jsonButton2.addEventListener('click', fetchJSON);
+jsonButton2.addEventListener('click', fetchTags);
 window.addEventListener('load', registerServiceWorker);
+
+// Check online status
+window.addEventListener("load", () => {
+	hasNetwork(navigator.onLine);
+  });
+  
+function hasNetwork(online) {
+
+	if (online) {
+		status.classList.remove("offline");
+		status.classList.add("online");
+		status.innerText = "Online";
+	} else {
+		status.classList.remove("online");
+		status.classList.add("offline");
+		status.innerText = "Offline";
+	}
+}
+
+window.addEventListener("load", () => {
+
+	hasNetwork(navigator.onLine);
+  
+	window.addEventListener("online", () => {
+		
+		hasNetwork(true);
+
+	});
+  
+	window.addEventListener("offline", () => {
+	
+		hasNetwork(false);
+
+	});
+  });
 
 // Error handling 
 function logError(error) {
 
-	console.log('Looks like there was a problem:', error);
+	let li = createNode('li');
+	let span = createNode('span');
+	span.innerHTML = `You're offline, can\'t fetch tags!`;
+	append(li, span);
+	append(ul, li);
 
 }
 
@@ -31,27 +76,56 @@ function append(parent, el) {
 }
 
 // Fetch the JSON data!
-function fetchJSON() {
+async function fetchJSON() {
 
-	fetch("https://cmgt.hr.nl/api/projects")
-	// console.log( data );
-	.then( response => response.json() )
-	.then( data => {
+	let responseJson;
 
-		let projects = data.data;
-		// console.log( projects )
-		return projects.map(function(project) {
-			// console.log(project);
-			let li = createNode('li');
-			let span = createNode('span');
-			span.innerHTML = `${project.project.title}`;
-			append(li, span);
-			append(ul, li);
-		} ) 
+	try {
+
+		const response = await fetch("https://cmgt.hr.nl/api/projects");
+		responseJson = await response.json();
+
+	} catch (error) {
+
+		responseJson = await localForage.getItem("projects") ;
+
+	}
+
+	ul.innerHTML = responseJson.data.map(createProject).join('\n');
 	
-	} ) 
-	.catch(logError);
+}
 
+function createProject(project){
+	console.log(project)
+	return `
+		<li class="project">
+			<div class="project__img">
+				<img src="${project.project.header_image}">
+			</div>
+			<div class="project__title">
+				<p>${project.project.title}</p>
+			</div>
+		</li> 
+	`;
+
+}
+
+// Fetch the tags!
+async function fetchTags() {
+
+	const response = await fetch("https://cmgt.hr.nl/api/tags").catch(logError);
+	const responseJson = await response.json();
+
+	let tags = responseJson.data;
+	return tags.map(function(tag) {
+		let li = createNode('li');
+		let span = createNode('span');
+		console.log(tag.name);
+		span.innerHTML = `${tag.name}`;
+		append(li, span);
+		append(ul, li);
+
+	} ) 
 }
 
 // Register service worker
@@ -65,3 +139,8 @@ function registerServiceWorker() {
   	}
 
 } 
+
+// Check if IndexedDB is supported by browser
+if ( !( 'indexedDB' in window ) ) {
+	console.log('This browser doesn\'t support IndexedDB');
+}
--- pwa/serviceWorker.js
+++ pwa/serviceWorker.js
@@ -1,11 +1,16 @@
-const staticCacheName = "site-static-v2";
+
+// Imports
+import localForage from "localforage";
+
+// Variables
+const staticCacheName = "site-static-v1";
 
 // Caching our assets
 const assets = [
 	"/",
 	"/index.html",
-	"/css/style.css",
-	"/js/app.js",
+	"/style.78032849.css",
+	"/app.c3f9f951.js",
 	"https://fonts.googleapis.com/css2?family=Inter&display=swap"
 ];
 
@@ -28,20 +33,56 @@ self.addEventListener("activate", activateEvent => {
 		caches.keys().then(keys => {
 			// console.log(keys);
 			return Promise.all(keys
-				.filter(key => key !== staticCacheName)
+				.filter(key => key !== staticCacheName )
 				.map(key => caches.delete(key))
 			)
 		})
 	);
 });
 
-// Checking if cache matches fetch
+// Fetches requests
 self.addEventListener('fetch', fetchEvent => {
-	console.log("fetch event", fetchEvent);
-	fetchEvent.respondWith(
-		caches.match(fetchEvent.request)
-		.then(cacheResponse => {
-			return cacheResponse || fetch(fetchEvent.request);
-		})
-	);
+
+	const fetchUrl = "https://cmgt.hr.nl/api/projects";
+	console.log(fetchEvent.request.url)
+
+	if (fetchEvent.request.url == fetchUrl) {
+		
+		fetchEvent.respondWith( 
+			
+			networkFirst( fetchUrl )
+
+		)	
+			
+	} else {
+
+		fetchEvent.respondWith(
+		
+			caches.match(fetchEvent.request)
+			.then(function (response) {
+				return response || fetch(fetchEvent.request);
+			})
+		);
+
+	}
+
 });
+
+async function networkFirst( fetchUrl) {
+	
+	try {
+			
+		const res = await fetch(fetchUrl);
+		const data = await res.clone().json();
+
+		localForage.setItem("projects", data);
+		return res;
+	
+	} catch( error ) {
+
+		const storage = await localForage.getItem("projects");
+		return new Response(storage);
+
+	}	
+
+}
